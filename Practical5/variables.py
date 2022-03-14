7068ddd2bdd52ade0639652edc51a6b2cd9e8d95
ZJEBMI2101zhuxinlu <xinluz.21@intl.zju.edu.cn>
2022-03-14 21:35:59 +0800

    modify variables

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -3,17 +3,18 @@ b= 4218520
 c= 271
 d= b-c
 e= a-b
-
+print ("the difference between the numbers of cases in 2020 and 2021 is",d)
+print ("the difference between the numbers of cases in 2021 and 2022 is",e)
 if d<<e:
  print ("e is greater")
 else:
  print ("d is greater")
-
 #e is greater. So 2021 has the greatest number of new COVID-19 cases.
 
 f= d/c
 g= e/b
-
+print (f)
+print (g)
 if f<<g:
  print ("rate in 2021 is greater")
 else:
