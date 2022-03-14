e2b01b52e4e86475db96548a0cd1c0c6798322d9
ZJEBMI2101zhuxinlu <xinluz.21@intl.zju.edu.cn>
2022-03-14 16:25:53 +0800

    do triangle sequence

---
+++ Practical5/triangle.py
@@ -0,0 +1,7 @@
+triangle_number=0
+#define triangle number.Set the initial value of triangle number
+for i in range (1,11):
+ triangle_number= triangle_number + i
+#increase the lengths of the sides of the triangle in turn and calculate the total
+ print ("triangle number",i,": ",triangle_number)
+#print the results
