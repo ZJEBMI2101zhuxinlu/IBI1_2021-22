cdf2d4ab3539d561e3f7d66c78b95c21e563a0fd
ZJEBMI2101zhuxinlu <xinluz.21@intl.zju.edu.cn>
2022-03-16 13:24:02 +0800

    do variables

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -6,14 +6,19 @@ d= b-c
 e= a-b
 print ("the difference between the numbers of cases in 2020 and 2021 is",d)
 print ("the difference between the numbers of cases in 2021 and 2022 is",e)
-if d<<e:
+if d<e:
  print ("e is greater")
 else:
  print ("d is greater")
 #e is greater. So 2021 has the greatest number of new COVID-19 cases.
 
 #3.2 booleans
-X="x"
-Y="y"
+X= True
+Y= False
 W= X and Y
+print (W)
+
+X= True
+Y= False
+W= X or Y
 print (W)
