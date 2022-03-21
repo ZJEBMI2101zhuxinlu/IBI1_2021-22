508bc0707bdfa009c7814d1909c064d143781f1f
ZJEBMI2101zhuxinlu <xinluz.21@intl.zju.edu.cn>
2022-03-21 20:52:35 +0800

    do list manipulation

--- Practical6/list_manipulation.py
+++ Practical6/list_manipulation.py
@@ -1,6 +1,8 @@
+#print a list of sorted	values for Rob’s marks across the semester
 marks= [45,36,86,57,53,92,65,45]
 print (sorted(marks))
 
+#a boxplot displaying the distribution of marks
 import matplotlib.pyplot as plt
 plt.boxplot(marks,
             vert = True,
@@ -15,6 +17,7 @@ plt.boxplot(marks,
 plt.ylabel('scores')
 plt.show()
 
+#the average mark Rob has received across the eight practicals
 avg = sum(marks)/len(marks)
 print (avg)
 if avg>=60:
