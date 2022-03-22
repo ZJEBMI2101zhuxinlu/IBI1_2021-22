c0221cc70b974db790aef0e8085074a391edde94
ZJEBMI2101zhuxinlu <xinluz.21@intl.zju.edu.cn>
2022-03-22 20:47:44 +0800

    do offspring health

--- Practical6/offspring_health.py
+++ Practical6/offspring_health.py
@@ -1,12 +1,13 @@
 paternal_age=[30,35,40,45,50,55,60,65,70,75]
 chd=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
 #print dictionary
-dir= {'30':1.03, '35':1.07, '40':1.11, '45':1.17, '50':1.23, '55':1.32, '60':1.42, '65':1.55, '70':1.72, '75':1.94}
+dir= {}
+for i in range(len(paternal_age)):
+ dir[paternal_age[i]]= chd[i]
 print (dir)
 
-age=30 #input paternal age
-risk= chd[int(age/5-6)]
-print ("The risk of CHD in the offspring of a father of",age,"is",risk)
+age= input ("paternal age: ") #input paternal age
+print ("The risk of CHD in the offspring of a father of",age,"is",dir[int(age)])
 
 #construct a scatter plot from the data
 import matplotlib.pyplot as plt
