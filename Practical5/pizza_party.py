17e77e6a57a4aa41511563cc1d20881a518704cc
ZJEBMI2101zhuxinlu <101116403+ZJEBMI2101zhuxinlu@users.noreply.github.com>
2022-04-20 00:07:42 +0800

    Add files via upload

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -1,6 +1,6 @@
 p=1 #define the number of pieces and start with 1
 n=0 #define the number of cuts and start with 0
-while p<64:
+while p<64: #when the number of pizza pieces is less than 64, keep cutting the pizza until there is enough for each IBI student
  n=n+1
  p=(n**2+n+2)/2
  print ("with straight cuts",n,", the number of pieces of piazza is",p) #the total number of pieces of pizza that can be cut for an increasing number of straight cuts until there are enough pieces for each member of the class
--- Practical6/offspring_health.py
+++ Practical6/offspring_health.py
@@ -2,17 +2,17 @@ paternal_age=[30,35,40,45,50,55,60,65,70,75]
 chd=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
 #print dictionary
 dir= {}
-for i in range(len(paternal_age)):
+for i in range(len(paternal_age)): #make a one-to-one correspondence between parental age and rate and form a dictionary
  dir[paternal_age[i]]= chd[i]
 print (dir)
 
 age= input ("paternal age: ") #input paternal age
-print ("The risk of CHD in the offspring of a father of",age,"is",dir[int(age)])
+print ("The risk of CHD in the offspring of a father of",age,"is",dir[int(age)]) #the relative rate for a given input paternal age 
 
 #construct a scatter plot from the data
 import matplotlib.pyplot as plt
-plt.scatter(paternal_age,chd, marker='*', color= 'lightpink')
-plt.title('parental age vs offspring health')
-plt.xlabel('parental age')
-plt.ylabel('offspring health')
+plt.scatter(paternal_age,chd, marker='*', color= 'lightpink') #define the marker and color of this plot
+plt.title('parental age vs offspring health') #define the title
+plt.xlabel('parental age') #define the xlable
+plt.ylabel('offspring health') #define the ylable
 plt.show()
