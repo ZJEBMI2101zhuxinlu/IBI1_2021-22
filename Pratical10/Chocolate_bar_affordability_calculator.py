ecd498449cc04b028508bc33aeb00ff356b070c4
ZJEBMI2101zhuxinlu <101116403+ZJEBMI2101zhuxinlu@users.noreply.github.com>
2022-04-20 18:56:35 +0800

    Add files via upload

--- Pratical10/Chocolate_bar_affordability_calculator.py
+++ Pratical10/Chocolate_bar_affordability_calculator.py
@@ -1,6 +1,6 @@
 def chocolate_calculator(total_money,price):
 	bar_number=total_money//price
 	left_money=total_money-bar_number*price
-	return bar_number,left_money
+	return (f'the number of chocolate is {bar_number}\nleft money is {left_money}')
 #example
 print(chocolate_calculator(100,7))
