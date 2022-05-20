35996479e37424293bf8871f423671b246c3228a
ZJEBMI2101zhuxinlu <101116403+ZJEBMI2101zhuxinlu@users.noreply.github.com>
2022-05-20 19:07:47 +0800

    Add files via upload

---
+++ Practical9/get_cut_genes.py
@@ -0,0 +1,22 @@
+import re
+import os
+os.chdir("/Users/86183/IBI1/IBI1_2021-22/Practical8")
+file=open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
+output=open('cut_genes.fa','w')
+se = file.read()
+se = ''.join(se.split('\n'))
+se=se.lstrip('>')
+gene=re.split(r'>',se)
+result=''
+for i in range(len(gene)):
+	if re.search(r'GAATTC',gene[i]):
+		a=str(re.findall(r'gene:(.+?)\s',gene[i]))
+		a=a.strip("['']")
+		b=str(re.findall(r'](.+)',gene[i]))
+		b=b.strip("['']")
+		c=len(b)
+		result=result+'>'+f'{a:10}'+str(c)+'\n'+b+'\n'
+result=result[:-1]
+output.write(result)
+file.close()
+output.close()
