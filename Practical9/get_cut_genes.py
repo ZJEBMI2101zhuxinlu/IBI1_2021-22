c786b6e83d42d1b0a26a035d8928f6d0ecf0b3f6
ZJEBMI2101zhuxinlu <101116403+ZJEBMI2101zhuxinlu@users.noreply.github.com>
2022-05-22 22:46:23 +0800

    Add files via upload

--- Practical9/get_cut_genes.py
+++ Practical9/get_cut_genes.py
@@ -1,6 +1,6 @@
 import re
 import os
-os.chdir("/Users/86183/IBI1/IBI1_2021-22/Practical8")
+os.chdir("/Users/86183/IBI1/IBI1_2021-22/Practical9")
 file=open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
 output=open('cut_genes.fa','w')
 se = file.read()
