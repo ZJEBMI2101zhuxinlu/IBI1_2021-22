5ad8e1263db6f879ff2fa0ef278b0d69029caae9
ZJEBMI2101zhuxinlu <101116403+ZJEBMI2101zhuxinlu@users.noreply.github.com>
2022-05-24 18:59:04 +0800

    make the codes clearer

--- Practical9/fragment_counter.py
+++ Practical9/fragment_counter.py
@@ -1,4 +1,6 @@
 seq='ATGCAATCGACTACGATCAATCGAGGGCC'
+seqence_cut=seq.count('GAATTC')
 number=seq.count('GAATTC')+1
-print(number)
-print('There is no enzymatic sequence and the generated DNA fragment is its own.')
+print('the number of the sequnce that can be cut:',seqence_cut)
+print('the number of fragments which could be generated:',number)
+#There is no enzymatic sequence and the generated DNA fragment is its own.
