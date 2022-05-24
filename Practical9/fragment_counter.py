097406ac99663bec749b5582dfb6b568e7a4bd4d
ZJEBMI2101zhuxinlu <101116403+ZJEBMI2101zhuxinlu@users.noreply.github.com>
2022-05-24 16:59:49 +0800

    Add more comment

--- Practical9/fragment_counter.py
+++ Practical9/fragment_counter.py
@@ -1,4 +1,4 @@
 seq='ATGCAATCGACTACGATCAATCGAGGGCC'
 number=seq.count('GAATTC')+1
 print(number)
-#There is no enzymatic sequence and the generated DNA fragment is its own.
+print('There is no enzymatic sequence and the generated DNA fragment is its own.')
