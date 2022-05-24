ef5c8c2e0720bac52b31898ca3908f41c6260052
ZJEBMI2101zhuxinlu <101116403+ZJEBMI2101zhuxinlu@users.noreply.github.com>
2022-05-24 13:03:15 +0800

    Add some pseudocode

--- Practical6/offspring_health.py
+++ Practical6/offspring_health.py
@@ -1,10 +1,12 @@
 paternal_age=[30,35,40,45,50,55,60,65,70,75]
 chd=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
-#print dictionary
-dir= {}
-for i in range(len(paternal_age)): #make a one-to-one correspondence between parental age and rate and form a dictionary
- dir[paternal_age[i]]= chd[i]
-print (dir)
+
+dir= {} #first define an empty dictionary
+for i in range(len(paternal_age)): 
+#by for loop, make a one-to-one correspondence between parental age and rate and form a dictionary
+#make the paternal_age the key and make the chd the value
+ dir[paternal_age[i]]= chd[i] #add a key-value pair in the dictionary
+print (dir) #print dictionary
 
 age= input ("paternal age: ") #input paternal age
 print ("The risk of CHD in the offspring of a father of",age,"is",dir[int(age)]) #the relative rate for a given input paternal age 
