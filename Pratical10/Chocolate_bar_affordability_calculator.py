e1574d8b7c2a6c0d33ff9d7e8aac7ec02ece99b2
ZJEBMI2101zhuxinlu <101116403+ZJEBMI2101zhuxinlu@users.noreply.github.com>
2022-05-25 11:21:58 +0800

    revise the code

--- Pratical10/Chocolate_bar_affordability_calculator.py
+++ Pratical10/Chocolate_bar_affordability_calculator.py
@@ -1,7 +1,9 @@
-import string
 def chocolate_calculator(total_money,price):
 	bar_number=total_money//price
 	left_money=total_money-bar_number*price
 	return (f'the number of chocolate is {bar_number}\nleft money is {left_money}')
 #example
-print(chocolate_calculator(100,7)) #you can change the total number and price here
+#you can change the total number and price here
+total_money=100
+price=7
+print(chocolate_calculator(total_money,price))
