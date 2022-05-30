c764051b1dbcb32030abfba71f4abf6098bdd6c0
ZJEBMI2101zhuxinlu <101116403+ZJEBMI2101zhuxinlu@users.noreply.github.com>
2022-05-30 14:36:49 +0800

    make more beautiful boxplots

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -8,7 +8,7 @@ os.chdir("C:/Users/86183/IBI1/IBI1_2021-22/Practical7") #the marker can change t
 covid_data=pd.read_csv("full_data.csv")
 
 #the first and third columns from rows 10-20
-print(covid_data.iloc[9:20,0:3:2]) #or[10:21]. Rob said they are all right:)
+print(covid_data.iloc[9:20,0:3:2]) # or[10:21]. Rob said they are all right:)
 
 #"total cases" for all rows corresponding to Afghanistan
 my_columns = [False, False, False, False, True, False]
@@ -23,7 +23,6 @@ print(covid_data.loc[my_rows,my_columns])
 # the mean number of new cases and new deaths in China
 # the proportion of new cases that kill the infected person
 china_new_data=covid_data.loc[covid_data['location']=='China',['date','new_cases','new_deaths']]
-print (china_new_data)
 china_dates=china_new_data.iloc[:,0]
 new_cases_mean=np.mean(china_new_data["new_cases"])
 print('the mean number of new cases:',new_cases_mean)
@@ -41,11 +40,11 @@ plt.ylabel('number')
 plt.show()
 
 plt.boxplot([x1,x2],labels=['new_cases','new_deaths'],showfliers=False) #here the outliers do not show to make the boxplots look more beautiful
-plt.title("the boxplots of new cases and new deaths in China worldwide without outliers")
+plt.title("the boxplots of new cases and new deaths in China worldwide\n(without outliers)")
 plt.ylabel('number')
 plt.show()
 
-# both new cases and new deaths in China over time
+#both new cases and new deaths in China over time
 x=china_dates
 y1=china_new_data["new_cases"]
 y2=china_new_data["new_deaths"]
