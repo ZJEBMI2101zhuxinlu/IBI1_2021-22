29f5ed84afb9300754c5b33933076b61b50f9f19
ZJEBMI2101zhuxinlu <101116403+ZJEBMI2101zhuxinlu@users.noreply.github.com>
2022-06-08 15:55:21 +0800

    revise the code

--- Pratical10/nucleotide_content_calculator.py
+++ Pratical10/nucleotide_content_calculator.py
@@ -12,14 +12,14 @@ def nucleotide_calculator(DNA):
 			C=C+1
 		if DNA[i].upper()=='G':
 			G=G+1
-	percentage_A=A/len(DNA)
-	percentage_T=T/len(DNA)
-	percentage_C=C/len(DNA)
-	percentage_G=G/len(DNA)
-	print('percentage_A is',percentage_A)
-	print('percentage_T is',percentage_T)
-	print('percentage_C is',percentage_C)
-	print('percentage_G is',percentage_G)
+	percentage_A=A/len(DNA)*100
+	percentage_T=T/len(DNA)*100
+	percentage_C=C/len(DNA)*100
+	percentage_G=G/len(DNA)*100
+	print('percentage_A is '+str(percentage_A)+'%')
+	print('percentage_T is '+str(percentage_T)+'%')
+	print('percentage_C is '+str(percentage_C)+'%')
+	print('percentage_G is '+str(percentage_G)+'%')
 DNA_example='atcgGGTACaTTGAtgccta'
 nucleotide_calculator(DNA_example)
 DNA=input('sequence:')
